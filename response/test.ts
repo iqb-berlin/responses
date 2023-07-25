eacc30399a15a925a57f958f23dc20c37d216f4e
mechtelm <mechtel@iqb.hu-berlin.de>
2023-07-25 11:49:03 +0200

    Add checks for invalid for response.json

--- response/test.ts
+++ response/test.ts
@@ -1,44 +1,82 @@
-const schema_filename = './response/response.json';
-const valid_folder = './response/test_valid';
+const schema_filename = `${__dirname}/response.json`;
+const valid_folder = `${__dirname}/test_valid`;
+const invalid_folder = `${__dirname}/test_invalid`;
 
 const Ajv = require("ajv")
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
 
 const fs = require('fs');
-console.log(`load schema "${schema_filename}"`);
+let compiledSchema: { (arg0: any): any; errors: any; } | null = null;
+
+function evaluateFileOk(filename: string, checkForInvalid: boolean): boolean {
+    try {
+        const data = fs.readFileSync(filename, 'utf8');
+        const valid = compiledSchema ? compiledSchema(JSON.parse(data)) : null;
+        if (valid) {
+            if (checkForInvalid) {
+                console.log(`\x1b[0;31mERROR\x1b[0m '${filename}' should be invalid`);
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            if (checkForInvalid) {
+                return true;
+            } else {
+                console.log(`\x1b[0;31mERROR\x1b[0m '${filename}':`);
+                console.error(compiledSchema ? compiledSchema.errors : 'error unknown')
+                return false;
+            }
+        }
+    } catch (err) {
+        console.log(`\x1b[0;31mERROR\x1b[0m '${filename}':`);
+        console.error(err);
+        return false;
+    }
+}
+
 let schema = null;
 try {
     schema = fs.readFileSync(schema_filename, 'utf8');
 } catch (err) {
+    console.log(`\x1b[0;31mERROR\x1b[0m reading schema '${schema_filename}':`);
     console.error(err);
+    process.exitCode = 1;
     schema = null;
 }
 
 if (schema) {
-    console.log('validate schema syntax');
-    let compiledSchema = null;
     try {
         compiledSchema = ajv.compile(JSON.parse(schema))
     } catch (err) {
-        compiledSchema = null;
+        console.log(`\x1b[0;31mERROR\x1b[0m parsing schema '${schema_filename}':`);
         console.error(err);
+        process.exitCode = 1;
+        compiledSchema = null;
     }
     if (compiledSchema) {
-        fs.readdirSync(valid_folder).forEach((file) => {
-            console.log(`evaluating ${file}`);
-            try {
-                const data = fs.readFileSync(`${valid_folder}/${file}`, 'utf8');
-                const valid = compiledSchema(JSON.parse(data))
-                if (valid) {
-                    console.log('ok');
-                } else {
-                    console.error(compiledSchema.errors)
-                }
-            } catch (err) {
-                console.error(err);
+        let checksOk = 0;
+        let checksNotOk = 0;
+        fs.readdirSync(valid_folder).forEach((file: string) => {
+            if (evaluateFileOk(`${valid_folder}/${file}`, false)) {
+                checksOk += 1;
+            } else {
+                checksNotOk += 1;
             }
         });
+        fs.readdirSync(invalid_folder).forEach((file: string) => {
+            if (evaluateFileOk(`${invalid_folder}/${file}`, true)) {
+                checksOk += 1;
+            } else {
+                checksNotOk += 1;
+            }
+        });
+        if (checksNotOk > 0) {
+            console.log(`\x1b[0;31m${checksNotOk} check${checksNotOk > 1 ? 's' : ''} failed\x1b[0m`);
+            process.exitCode = 1;
+        }
+        if (checksOk > 0) {
+            console.log(`\x1b[0;32m${checksOk} check${checksOk > 1 ? 's' : ''} passed\x1b[0m`);
+        }
     }
 }
-console.log('test finished');
-
