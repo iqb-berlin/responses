8a5c11e2314779ba77784d770b283d5adfca710f
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-03-19 10:34:28 +0100

    Export coding-factory

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './coding-interfaces';
 export * from './variable-list';
 export * from './coding-scheme-factory';
 export * from './to-text-factory';
+export * from './coding-factory';
